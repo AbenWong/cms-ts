4cb4580f84661dd8035aac550920c1487ea6cd26
ohsogasne <1263254562@qq.com>
2021-08-26 17:31:56 +0800

    fix: have err

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,15 @@
 <template>
-  <div>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="hi!你好" />
+  <div id="app">
+    <router-link to="/login">登录</router-link>
+    <router-link to="/main">首页</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
-import HelloWorld from './components/HelloWorld.vue'
-
 export default defineComponent({
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+  name: 'App'
 })
 </script>
 
---
+++ vue.config.js
@@ -0,0 +1,23 @@
+const path = require('path')
+module.exports = {
+  //配置方式一：CLI提供属性
+  outputDir: './build', // 打包位置
+  //配置方式二：与webpack相同，最后被合并到一起
+  // 这里是个对象，相当于用webpack-merger进行合并
+  //为了在其他文件中引入路径起一个别名，方便后续引入
+  /*     configureWebpack: {
+            resolve: {
+                //别名
+                alias: {
+                    // 默认情况下，@对应在src
+                    component: '@/component'
+                }
+            }
+        } */
+  //  第三种方式，通过函数进行引入，相同的被覆盖，意思是别名为@去代替当前目录下的src文件路径，views代替src/views
+  chainWebpack: (config) => {
+    config.resolve.alias
+      .set('@', path.resolve(__dirname, 'src'))
+      .set('views', '@/views')
+  }
+}
