3d428cc911d2ac27a626b52e1a86efbe4b42c4d8
ohsogasne <1263254562@qq.com>
2021-08-26 23:22:21 +0800

    fix: element

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,11 +3,11 @@ module.exports = {
   env: {
     node: true
   },
-  /*    '@vue/typescript/recommended ', */
+
   extends: [
     'plugin:vue/vue3-essential',
     'eslint:recommended',
-	'@vue/typescript/recommended ',
+    '@vue/typescript/recommended',
     '@vue/prettier',
     '@vue/prettier/@typescript-eslint',
     'plugin:prettier/recommended'
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,24 +1,26 @@
+//引入路由
 import { createRouter, createWebHashHistory } from 'vue-router'
+//引入路由类型检测
 import { RouteRecordRaw } from 'vue-router'
 
 const routes: RouteRecordRaw[] = [
   {
     path: '/',
-    redirect: '/main'
+    redirect: '/main' //重定向，
   },
   {
     path: '/main',
-    component: () => import('../views/main/main.vue')
+    component: () => import('../views/main/main.vue') //别名
   },
   {
     path: '/login',
     component: () => import('../views/login/login.vue')
   }
 ]
-
+//创建router对象
 const router = createRouter({
   routes,
-  history: createWebHashHistory()
+  history: createWebHashHistory() //hash模式，带#的
 })
 
 export default router
